affe2431851b9a486bf644dc2d8656979fd0fc7a
Alextran0899 <Alextran0899@gmail.com>
2023-03-28 22:16:38 -0500

    intial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import './App.css';
+import io from 'socket.io-client'
+import {useEffect, useState} from "react";
+
+const socket = io.connect(process.env.REACT_APP_API_URL)
+
+function App() {
+    const [isOn, setIsOn] = useState(false)
+
+    useEffect(() => {
+        console.log(process.env.REACT_APP_API_URL)
+        socket.on("light_bulb_state_to_client", (data) => {
+            console.log(data.light_bulb_is)
+            setIsOn(data.light_bulb_is)
+        })
+        socket.on("light_bulb_state_to_client_from_joseph", (data) => {
+            if(data.light_bulb_is) {
+                alert("Congratulation Joesph, you turn the light bulb on without turning it on")
+            }
+            setIsOn(data.light_bulb_is)
+        })
+    }, [])
+
+    const sendState = (light_bulb_curr_state) => {
+        socket.emit("light_bulb_state_to_server", {light_bulb_is: light_bulb_curr_state})
+    }
+
+    const onClick = () => {
+        const light_bulb_curr_state = !isOn
+        setIsOn(light_bulb_curr_state)
+        sendState(light_bulb_curr_state)
+    }
+
+    return (
+            <div className={isOn?"on" : ""} onClick={onClick}>
+            <div className="light Z">
+                <div className="wire"></div>
+                <div className="bulb">
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+            </div>
+    );
+}
+
+export default App;
